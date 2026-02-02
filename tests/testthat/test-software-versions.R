7a9fb2eefcad7e2b7fd643136bf91c676cf42d84
Colin Gillespie <colin@jumpingrivers.com>
2026-02-02 16:37:03 +0000

    fix: Adding end of life python versions - #28 (#43)
    * fix: Adding end of life python versions - fixes #28

--- tests/testthat/test-software-versions.R
+++ tests/testthat/test-software-versions.R
@@ -1,52 +1,77 @@
 test_that("Testing software versions", {
-  versions = get_latest_versions()
-  v_colnames = c("software", "version", "major", "patch", "version_num")
+  versions = get_latest_versions(remote = FALSE)
+  v_colnames = c("software", "version", "major", "patch", "version_num", "within_eol")
   expect_equal(colnames(versions), v_colnames)
   expect_gte(nrow(versions), 16)
+})
 
-  installed = tibble::tibble(
-    software = c("r", "r", "python"),
-    installed_version = c("3.4.3", "3.5.3", "3.7.1")
-  )
-  augmented = augment_installed(installed)
+test_that("Testing augment_installed (empty)", {
+  installed = tibble::tibble(software = character(0), installed_version = character(0))
+  augmented_empty = augment_installed(installed, remote = FALSE, verbose = FALSE)
   expect_equal(
-    colnames(augmented),
+    colnames(augmented_empty),
     c(
-      v_colnames[1:4],
+      "software",
+      "version",
+      "major",
+      "patch",
+      "within_eol",
       paste0("installed_", c("version", "patch")),
       "upgrade"
     )
   )
-  expect_equal(sum(!augmented$upgrade), 1)
-
-  # Check specific packages
-  augmented = augmented[!is.na(augmented$installed_version), ]
-  expect_equal(augmented$installed_patch, c(1, 3, 3))
-
-  # Test really old versions: Upgrade old version
-  installed = tibble::tibble(software = "r", installed_version = "1.5.3")
-  augmented = augment_installed(installed)
-  expect_equal(sum(augmented$upgrade), 10)
-
-  # Test version not in DB - don't upgrade
-  installed = tibble::tibble(software = "r", installed_version = "99.4.3")
-  augmented = augment_installed(installed)
-  expect_equal(sum(augmented$upgrade), 9)
 })
 
-test_that("Testing software versions quarto output", {
+test_that("Testing augment_installed (old)", {
+  augmented_empty =
+    augment_installed(
+      tibble::tibble(
+        software = character(0),
+        installed_version = character(0)
+      ),
+      remote = FALSE,
+      verbose = FALSE
+    )
+
+  # All really old versions
   installed = tibble::tibble(
-    software = c("r", "r", "python"),
-    installed_version = c("3.4.3", "3.5.3", "3.7.1")
+    software = c("r", "python"),
+    installed_version = c("2.4.1", "3.2.1")
   )
-  augmented = suppressMessages(augment_installed(installed))
-  out = list(versions = augmented)
-  q = get_quarto_software_versions(out)
+  augmented = augment_installed(installed, remote = FALSE, verbose = FALSE)
+  expect_equal(nrow(augmented) - nrow(augmented_empty), 2)
+  expect_equal(sum(!augmented$upgrade), 0)
+
+  installed_augmented_versions = dplyr::left_join(
+    installed,
+    augmented,
+    by = dplyr::join_by(software, installed_version)
+  )
   expect_equal(
-    colnames(q),
-    c("software", "version", "installed_version", "upgrade")
+    nrow(installed_augmented_versions),
+    2
   )
-  expect_true(all(is.logical(q$upgrade)))
+
+  expect_equal(sum(installed_augmented_versions$within_eol), 0)
+  expect_equal(sum(installed_augmented_versions$upgrade), 2)
+})
+
+test_that("Testing augment_installed (new)", {
+  # Test really old versions: Upgrade old version
+  installed = tibble::tibble(software = "r", installed_version = "4.4.3")
+  augmented = augment_installed(installed, remote = FALSE, verbose = FALSE)
+  augmented = augmented[augmented$version == "4.4.3", ]
+  expect_equal(nrow(augmented), 1)
+  expect_false(augmented$upgrade)
+})
+
+test_that("Testing augment_installed (future))", {
+  # Test version not in DB - don't upgrade
+  installed = tibble::tibble(software = "r", installed_version = "99.4.3")
+  augmented = augment_installed(installed, remote = FALSE, verbose = FALSE)
+  augmented = augmented[augmented$version == "99.4.3", ]
+  expect_equal(nrow(augmented), 1)
+  expect_false(augmented$upgrade)
 })
 
 test_that("Ensure that software versions are up to date", {
